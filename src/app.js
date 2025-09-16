33ce5ded9de3cf51564014eff3e9989ab79b3131
Raghav Kapoor <raghavkapoor16947@gmail.com>
2025-09-16 17:56:38 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 const corsOptions = {
-    origin: ['http://localhost:5000' , 'https://raghav-0202.github.io'],
+    origin: ['http://localhost:5000' , 'https://raghav-0202.github.io' , 'https://rockfallsih.netlify.app'  , "https://www.rockguard.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
     credentials: true ,
